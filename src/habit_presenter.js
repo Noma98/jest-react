c4a1b1ad8fe21d5e853e0b160317f7dbf75a116f
jinhee <wiostz98kr@gmail.com>
2022-06-29 20:41:30 +0900

    add: habit_presenter 테스트 코드 작성(기능, 오브젝트 불변성, 최대값 제한, 에러 테스트) 및 테스트 유틸 함수 구현

--- src/habit_presenter.js
+++ src/habit_presenter.js
@@ -1,6 +1,7 @@
 export default class HabitPresenter {
-  constructor(habits) {
+  constructor(habits, maxHabits) {
     this.habits = habits;
+    this.maxHabits = maxHabits;
   }
   getHabits() {
     return this.habits;
@@ -17,8 +18,10 @@ export default class HabitPresenter {
   decrement(habit, update) {
     this.habits = this.habits.map((item) => {
       if (item.id === habit.id) {
-        const count = habit.count - 1;
+        const count = item.count - 1;
         return { ...habit, count: count < 0 ? 0 : count };
+        //habit.count-1을 할 경우 return 하는 부분을 다음과 같이 바꿔도 { ...habit, count}; 테스트가 실패하지 않음(비정상)
+        //this.habits의 오브젝트를 사용하도록 변경하면 테스트가 실패함(정상)
       }
       return item;
     });
@@ -29,6 +32,9 @@ export default class HabitPresenter {
     update(this.habits);
   }
   add(name, update) {
+    if (this.habits.length === this.maxHabits) {
+      throw new Error(`습관의 갯수는 ${this.maxHabits} 이상이 될 수 없습니다.`);
+    }
     this.habits = [...this.habits, { name, id: Date.now(), count: 0 }];
     update(this.habits);
   }
