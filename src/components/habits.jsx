1781899f1f6b74b83288606b0b252dbcd89359c8
Noma <wiostz98kr@gmail.com>
2022-07-03 21:47:27 +0900

    move: habitAddForm을 components/form으로 이동

--- src/components/habits.jsx
+++ src/components/habits.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
+import HabitAddForm from './form/habitAddForm';
 import Habit from './habit';
-import HabitAddForm from './habitAddForm';
 
 const Habits = ({ habits, onIncrement, onDecrement, onDelete, onAdd, onReset }) => {
   return (
