6ee5c3e78130310a931d283847f58d135ea96960
Noma <wiostz98kr@gmail.com>
2022-07-03 22:22:03 +0900

    add: habitAddForm 스냅샷 테스트 추가(renderer.create, toMatchSnapshot)

---
+++ src/components/form/tests/habitAddForm.test.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import HabitAddForm from '../habitAddForm';
+import userEvent from '@testing-library/user-event';
+import renderer from 'react-test-renderer'
+
+describe("HabitAddForm", () => {
+    it('renders', () => {
+        const component = renderer.create(<HabitAddForm onAdd={jest.fn()} />)
+        expect(component.toJSON()).toMatchSnapshot();
+    });
+    describe('Form Submit', () => {
+        let onAdd;
+        let input;
+        let button;
+        beforeEach(() => {
+            onAdd = jest.fn();
+            render(<HabitAddForm onAdd={onAdd} />);
+            input = screen.getByPlaceholderText('Habit');
+            button = screen.getByText('Add2');
+        })
+        it('calls onAdd when button is clicked and valid habit is entered', () => {
+            userEvent.type(input, 'New Habit');
+            userEvent.click(button);
+
+            expect(onAdd).toHaveBeenCalledWith('New Habit');
+        });
+        it('doen not call onAdd when the habit is empty', () => {
+            userEvent.type(input, '');
+            userEvent.click(button);
+
+            expect(onAdd).toHaveBeenCalledTimes(0);
+        })
+    })
+
+})
